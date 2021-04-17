b4f68e8e6f396d0282b71378df9777a8438d0a1b
thomasguy <twguy66@gmail.com>
2021-04-17 15:41:09 +0700

    working template

--- gatsby-front/gatsby-config.js
+++ gatsby-front/gatsby-config.js
@@ -32,7 +32,7 @@ module.exports = {
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
     'gatsby-plugin-react-helmet',
-    // 'gatsby-plugin-sitemap',
+    'gatsby-plugin-sitemap',
     'gatsby-plugin-react-svg',
     {
       resolve: `gatsby-plugin-typography`,
